45e66463460dcf90a0b0d301912b868f10b57e5d
ArgTang <ArgTang@users.noreply.github.com>
2016-10-17 11:14:08 +0200

    Added Identity (#20)
    * Login logout
    
    * Entity v1 registration works, login does not
    
    * fix
    
    * Login logout funker!!
    
    * Cleaning 
    
    And moved seeding to startup.cs instead of triggering each time we visit homepage
    
    * Moved logout to loggedinController

--- src/GroupProject/Data/Seeddata.cs
+++ src/GroupProject/Data/Seeddata.cs
@@ -10,8 +10,9 @@ namespace GroupProject.Data
 {
     public static class SeedData
     {
-        public static void Seed(PersonDbContext context)
+        public static void SeedPersons(PersonDbContext context)
         {
+            context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
             if (!context.Person.Any())
