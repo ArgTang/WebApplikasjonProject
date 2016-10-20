43ca8860a7a8ba4c67aa38fc9011ec2575199f61
Ole <olelundsor@gmail.com>
2016-10-20 13:56:59 +0200

    fix

--- src/GroupProject/DAL/DbAccess.cs
+++ src/GroupProject/DAL/DbAccess.cs
@@ -1,8 +1,5 @@
 ﻿using GroupProject.Models;
-<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
-=======
->>>>>>> 0f251ae4673f753d701b647a0f3dc909f2031787
 using System;
 using System.Collections.Generic;
 using System.Linq;
