551f91632c72c097623afd97bd73b71c05452ce9
cafj <catoajohansen@hotmail.com>
2016-10-20 12:05:40 +0200

    Startinlogging (#28)
    * gui
    
    * gui
    
    * Renamed Controller
    
    * Fix rename Controller
    
    * fix logout path
    
    * siste oppdatering
    
    * fakturaoversikt
    
    * Fix logou button. needs styling
    
    * farger og knapper
    
    * Added _ViewStart to User Views
    
    * add paymentModel. inserted paymentModel into Betaling
    
    * remoed Bower deps, since we use CDN for everything
    
    * styling
    
    * transition on hover
    
    * noen endringer faktura og navigasjon
    
    * fix layout
    
    * remove layout

---
+++ src/GroupProject/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using GroupProject.Models;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+namespace GroupProject.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private PersonDbContext _persondbcontext { get; set; }
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UserController(SignInManager<ApplicationUser> signInManager, PersonDbContext persondbcontext)
+        {
+            this._signInManager = signInManager;
+            _persondbcontext = persondbcontext;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Logged in ACOS";
+            var konto = _persondbcontext.Kontoer.ToList();
+            return View(konto);
+        }
+
+
+        public IActionResult Faktura()
+        {
+            ViewData["Title"] = "Logged in ACOS";
+            return View();
+        }
+
+        public IActionResult Betal()
+        {
+            ViewData["Title"] = "Logged in ACOS";
+            return View();
+        }
+
+        public IActionResult Oversikt()
+        {
+            ViewData["Title"] = "Logged in ACOS";
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Betal()
+        //{
+        //    ViewData["Title"] = "Logged in ACOS";
+        //    return View();
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
