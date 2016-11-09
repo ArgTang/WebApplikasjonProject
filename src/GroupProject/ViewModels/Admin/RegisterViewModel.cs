1bb9e685cdc4b647c788297a00b690e9112302ce
Cato <catojohansen87@gmail.com>
2016-11-09 18:50:48 +0100

    wip4

--- src/GroupProject/ViewModels/Admin/RegisterViewModel.cs
+++ src/GroupProject/ViewModels/Admin/RegisterViewModel.cs
@@ -2,36 +2,33 @@
 ﻿using System.Collections.Generic;
 using GroupProject.DAL;
 using System.ComponentModel.DataAnnotations;
-﻿using System.ComponentModel.DataAnnotations;
-
+using GroupProject.Annotations;
 
 namespace GroupProject.ViewModels.Admin
 {
     public class RegisterViewModel
     {
-
         //fornavn
+        [Required(ErrorMessage = "Dette feltet må være utfylt")]
         [DataType(DataType.Text)]
         [Display(Name = "Vennligst skriv inn ditt fornavn", Prompt = "Fornavn")]
-        [Required(ErrorMessage = "Dette feltet må være utfylt")]
         [StringLength(50, ErrorMessage = "Navnet kan maksimum ha 50 tegn"), MinLength(2, ErrorMessage = "Navnet må minimum ha to tegn")]
-        [RegularExpression("/^[a-z æøå]+$/i", ErrorMessage = "Navnet kan kun inneholde bokstaver eller mellomrom")]
+        [RegularExpression("[A-za-z æøåÆØÅ]+", ErrorMessage = "Navnet kan kun inneholde bokstaver eller mellomrom")]
         public string firstName { get; set; }
 
         //etternavn
+        [Required(ErrorMessage = "Dette feltet må være utfylt")]
         [DataType(DataType.Text)]
         [Display(Name = "Vennligst skriv inn ditt etternavn", Prompt = "Etternavn")]
-        [Required(ErrorMessage = "Dette feltet må være utfylt")]
         [StringLength(50, ErrorMessage = "Navnet kan maksimum ha 50 tegn"), MinLength(2, ErrorMessage = "Navnet må minimum ha to tegn")]
-        [RegularExpression("/^[a-z æøå]+$/i", ErrorMessage = "Navnet kan kun inneholde bokstaver eller mellomrom")]
+        [RegularExpression("[A-za-z æøåÆØÅ]+", ErrorMessage = "Navnet kan kun inneholde bokstaver eller mellomrom")]
         public string lastName { get; set; }
 
         //telefon
         [DataType(DataType.PhoneNumber)]
         [Display(Name = "Vennligst skriv inn ditt telefonnummer", Prompt = "Telefonnummer")]
         [Required(ErrorMessage = "Dette feltet må være utfylt")]
-        [StringLength(8, ErrorMessage = "Telefonnummeret kan kun inneholde 8 siffer")]
-        [RegularExpression("/^[0-9]{8}/", ErrorMessage = "Telefonnummeret kan kun inneholde 8 siffer")]
+        [RegularExpression("[0-9]{8}", ErrorMessage = "Telefonnummeret kan kun inneholde 8 siffer")]
         public string phonenumber { get; set; }
 
         //adresse
@@ -43,26 +40,44 @@ namespace GroupProject.ViewModels.Admin
 
         //postnummer
         [DataType(DataType.Text)]
-        [Display(Name = "Vennligst skriv inn ditt postnummer", Prompt = "Adresse")]
+        [Display(Name = "Vennligst skriv inn ditt postnummer", Prompt = "Postnummer")]
         [Required(ErrorMessage = "Dette feltet må være utfylt")]
-        [StringLength(4, ErrorMessage = "Postnummeret kan kun inneholde 4 siffer")]
-        [RegularExpression("/^[0-9]{4}/", ErrorMessage = "Telefonnummeret kan kun inneholde 8 siffer")]
+        [RegularExpression("[0-9]{4}", ErrorMessage ="Ett postnummer kan kun inneholde 4 siffer")]
         public string zipcode { get; set; }
 
         ////poststed
         //public string postal { get; set; }
         
-
+        //epostadresse
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Vennligst skriv inn din epostadresse", Prompt ="Epostadresse")]
         [Required(ErrorMessage ="Dette feltet må være utfylt")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage ="Epostadressen må inneholde @ og .")]
         public string epost { get; set; }
 
+        [Required(ErrorMessage ="Dette feltet må være utfylt")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Vennlist skriv inn ett passord", Prompt = "Passord")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{12,40}$", ErrorMessage = "Minimum 12 tegn, minst en bokstav og ett nummer")]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Dette feltet må være utfylt")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Vennlist skriv inn ett passord", Prompt = "Passord")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{12,40}$", ErrorMessage = "Minimum 12 tegn, minst en bokstav og ett nummer")]
+        [Compare("password", ErrorMessage="Passordet må være det samme som det du skrev i forrige felt")]
+        public string confirmPassword { get; set; }
+
+        //søk etter bruker
         [DataType(DataType.Text)]
         [Display(Name = "Vennligst skriv inn ett gyldig fødselsnummer", Prompt = "Fødselsnummer")]
         [Required(ErrorMessage = "Dette feltet må være utfylt")]
         [StringLength(11, ErrorMessage = "Ett gyldig fødselsnummer kan kun inneholde 11 siffer")]
-        [RegularExpression("/^[0-9]{11}/", ErrorMessage = "Ett gyldig fødselsnummer kan kun inneholde 11 siffer")]
-        public string sokBruker { get; set; }
+        [RegularExpression("[0-9]{11}", ErrorMessage = "Ett gyldig fødselsnummer kan kun inneholde 11 siffer")]
+        public string searchUser { get; set; }
+
+        //drop down med kontotyper
+        [Required(ErrorMessage = "Dette feltet må være utfylt")]
+        public string kontoType { get; set; }
     }
 }
