5ac3d9b460deeeb4df102914701118f23cdea145
ArgTang <ArgTang@outlook.com>
2016-11-09 17:51:18 +0100

    Test Funker

--- src/Test/Controller/AdminControllerTest.cs
+++ src/Test/Controller/AdminControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using GroupProject.Controllers;
+﻿using GroupProject.BLL;
+using GroupProject.Controllers;
 using GroupProject.DAL;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,37 +17,19 @@ namespace Test.Controller
         AdminController controller;
         public AdminControllerTest()
         {
-            var PersonDbContextMock = new Mock<PersonDbContext>();
-            var LoggerMock = new Mock<ILogger<DbAccess>>();
-            var dbStub = new DbAccessStub(PersonDbContextMock.Object, LoggerMock.Object);
-            var manager = MockUserManager<ApplicationUser>();
-            //Crash here
-            var adminBLLStub = new AdminBLLStub(manager.Object, dbStub);
-
-            controller = new AdminController(adminBLLStub);
+            var AdminMock = new Mock<AdminBLL>(null, null);
+            controller = new AdminController(AdminMock.Object);
         }
 
 
         [Fact]
         public void IndexTest()
         {
-            var result = controller.Index();
+            IActionResult result = controller.Index();
+            Assert.IsType<RedirectToActionResult>(result);
 
-            Assert.IsType<ViewResult>(result);
-        }
-
-
-        //https://github.com/aspnet/Identity/issues/640
-        public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
-        {
-            IList<IUserValidator<TUser>> UserValidators = new List<IUserValidator<TUser>>();
-            IList<IPasswordValidator<TUser>> PasswordValidators = new List<IPasswordValidator<TUser>>();
-
-            var store = new Mock<IUserStore<TUser>>();
-            UserValidators.Add(new UserValidator<TUser>());
-            PasswordValidators.Add(new PasswordValidator<TUser>());
-            var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, UserValidators, PasswordValidators, null, null, null, null, null);
-            return mgr;
+            var actionName = ((RedirectToActionResult) result).ActionName;
+            Assert.Equal("FakturaOversikt", actionName);            
         }
     }
 }
