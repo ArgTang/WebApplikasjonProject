5702e94be3f8985c725cec9f9c51961503b15e06
ArgTang <ArgTang@outlook.com>
2016-11-09 11:31:07 +0100

    temp

---
+++ src/Test/DAL/DbAccessStub.cs
@@ -0,0 +1,18 @@
+﻿using GroupProject.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Test.DAL
+{
+    public class DbAccessStub : DbAccess
+    {
+        public DbAccessStub(PersonDbContext personDbContext, ILogger<DbAccess> logger) : base(personDbContext, logger)
+        {
+        }
+
+
+    }
+}
