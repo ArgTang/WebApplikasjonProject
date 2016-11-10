ae9c4cd13f04abe48620b97a22230f44e279ca36
ArgTang <ArgTang@outlook.com>
2016-11-10 08:54:10 +0100

    fix 3

--- src/GroupProject/gulpfile.js
+++ src/GroupProject/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='compile' AfterBuild='js:default' Clean='clean' />
+/// <binding BeforeBuild='compile' Clean='clean' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
